0c34009d5183e7ad7a16742ddc71453dd5d7528e
Andrey Grunenkov <60237240+gras4all@users.noreply.github.com>
2022-10-08 13:43:31 +0400

    Initial Commit

---
+++ swiftui-combine-app/swiftui_combine_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_combine_appApp.swift
+//  swiftui-combine-app
+//
+//  Created by Андрей Груненков on 08.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_combine_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
